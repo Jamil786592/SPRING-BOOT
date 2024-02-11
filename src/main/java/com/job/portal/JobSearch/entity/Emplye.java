6d0474d681ce954d766e6d5ad9d22dc7d0c8fdae
sardarjahan92 <80624010+sardarjahan92@users.noreply.github.com>
2024-02-11 13:14:17 +0530

    JPA-FEB-11

--- src/main/java/com/job/portal/JobSearch/entity/Emplye.java
+++ src/main/java/com/job/portal/JobSearch/entity/Emplye.java
@@ -4,9 +4,16 @@ package com.job.portal.JobSearch.entity;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Entity
 @Table(name = "employe")
+@Data
+@Setter
+@Getter
 public class Emplye {
     @Id
     private String name;
